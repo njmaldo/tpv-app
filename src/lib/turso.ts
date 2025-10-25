91f0753924332666d43cb521d91f492704d77494
Néstor J. Maldonado <nestorjmaldo@gmail.com.com>
2025-10-25 10:17:56 +0800

    turso.ts

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -9,19 +9,10 @@ import { createClient } from '@libsql/client';
 // export default tursoClient;
 
 
-const url = import.meta.env.TURSO_DATABASE_URL;
-const authToken = import.meta.env.TURSO_AUTH_TOKEN;
-
-if (!url) {
-  throw new Error("TURSO_DATABASE_URL no está configurada.");
-}
-if (!authToken) {
-  throw new Error("TURSO_AUTH_TOKEN no está configurado.");
-}
 
 export const tursoClient = createClient({
-  url: url,
-  authToken: authToken,
+  url:import.meta.env.TURSO_DATABASE_URL as string,
+  authToken:import.meta.env.TURSO_AUTH_TOKEN as string,
 });
 
 
